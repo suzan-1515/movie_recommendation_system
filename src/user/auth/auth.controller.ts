6e3fd941cbebd33e116adebdcd9808558302084b
Sujan Parajuli <suzanparajuli@gmail.com>
2022-08-09 17:23:06 +0100

    verify token endpoint bug fixed

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -24,6 +24,7 @@ export class AuthController {
     }
 
     @Post('verify-token')
+    @UseGuards(JwtAuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
     private verifyToken(@Req() { user }: Request): Promise<{} | never> {
         return this.service.verifyToken(<User>user);
